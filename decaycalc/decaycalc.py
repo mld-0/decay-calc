906727c3c3135d155b6c28e4e0dcaba54061a0d4
Matthew L Davis <mld.0@protonmail.com>
2021-01-02 19:26:54 +1000

    get and read logfiles and tests
    Get list of files for given (monthly) date range, decrypt (to string),
    and place qtys and datetimes into returned lists.
    Fails to parse datetime format '(2021-01-02)-(19-28-21)'
    Basic (inadequate?) tests only.

--- decaycalc/decaycalc.py
+++ decaycalc/decaycalc.py
@@ -32,7 +32,6 @@ import dateutil.tz
 import dateutil.relativedelta 
 import time
 import tempfile
-import decimal
 import logging
 from subprocess import Popen, PIPE, STDOUT
 from os.path import expanduser
@@ -42,8 +41,11 @@ from subprocess import Popen, PIPE, STDOUT
 from io import StringIO
 from tzlocal import get_localzone
 from dateutil.relativedelta import relativedelta
+from decimal import Decimal
+import pandas
+import dateparser
 #   }}}1
-#   {{{1
+#   {{{2
 _log = logging.getLogger('decaycalc')
 _logging_format="%(funcName)s: %(levelname)s, %(message)s"
 _logging_datetime="%Y-%m-%dT%H:%M:%S%Z"
@@ -59,17 +61,89 @@ class DecayCalc(object):
     def Calculate(self, arg_dt_list, arg_dt_items, arg_qty_items, arg_halflife, arg_onset):
         pass
 
-    #   About: Get a list of the files in arg_data_dir, where each file is of the format 'arg_file_prefix.date_str.arg_file_postfix', and date_str is %Y-%m for each year and month between arg_dt_first and arg_dt_last
+    #   About: Get a list of the files in arg_data_dir, where each file is of the format 'arg_file_prefix + date_str + arg_file_postfix', and date_str is %Y-%m for each year and month between one month before arg_dt_first, and arg_dt_last
     def _GetFiles_Monthly(self, arg_data_dir, arg_file_prefix, arg_file_postfix, arg_dt_first, arg_dt_last):
-        pass
+    #   {{{
+        _dt_format_convertrange = '%Y-%m-%dT%H:%M:%S%Z'
+        _dt_format_output = '%Y-%m'
+        _dt_freq = 'MS'
+        arg_dt_beforeFirst = arg_dt_first + relativedelta(months = -1)
+        _log.debug("arg_dt_first=(%s)" % str(arg_dt_first))
+        _log.debug("arg_dt_beforeFirst=(%s)" % str(arg_dt_beforeFirst))
+        _log.debug("arg_dt_last=(%s)" % str(arg_dt_last))
+        dt_Range = [ x for x in pandas.date_range(start=arg_dt_beforeFirst.strftime(_dt_format_convertrange), end=arg_dt_last.strftime(_dt_format_convertrange), freq=_dt_freq) ]
+        dt_Range_str = [ x.strftime(_dt_format_output) for x in dt_Range ]
+        _log.debug("dt_Range=(%s)" % str(dt_Range))
+        _log.debug("dt_Range_str=(%s)" % str(dt_Range_str))
+        #   Raise exception if arg_data_dir doesn't exist
+        if not os.path.isdir(arg_data_dir):
+            raise Exception("dir not found arg_data_dir=(%s)" % str(arg_data_dir))
+        #   Get list of files of format 'arg_file_prefix + date_str + arg_file_postfix' which exist, with warning for those which don't. Raise exception if no files are found
+        located_filepaths = []
+        for loop_dt_str in dt_Range_str:
+            loop_candidate_filename = arg_file_prefix + loop_dt_str + arg_file_postfix
+            loop_candidate_filepath = os.path.join(arg_data_dir, loop_candidate_filename)
+            if os.path.isfile(loop_candidate_filepath):
+                located_filepaths.append(loop_candidate_filepath)
+        _log.debug("located_filepaths:\n%s" % pprint.pformat(located_filepaths))
+        if len(located_filepaths) == 0:
+            raise Exception("no files located")
+        return located_filepaths
+        #   }}}
 
     #   About: Given a list of files, get as a list qtys and datetimes (from columns arg_col_qty and arg_col_dt), for lines where value in column arg_col_label==arg_label (if arg_label is not None, otherwise read every line). Return [ results_dt, results_qty ] lists, sorted chronologicaly 
     def _ReadData(self, arg_files_list, arg_label, arg_col_dt, arg_col_qty, arg_col_label, arg_delim):
-        pass
+        _log.debug("arg_label=(%s)" % str(arg_label))
+        _log.debug("arg_delim=(%s)" % str(arg_delim))
+        _log.debug("cols: (dt, qty, label)=(%s, %s, %s)" % (arg_col_dt, arg_col_qty, arg_col_label))
+        results_dt = []
+        results_qty = []
+        for loop_filepath in arg_files_list:
+            loop_filestr = self._DecryptGPGFileToString(loop_filepath)
+            for loop_line in loop_filestr.split("\n"):
+                loop_line_split = loop_line.split(arg_delim)
+                #_log.debug("loop_line_split=(%s)" % str(loop_line_split))
+                if (arg_label is None) or (loop_line_split[arg_col_label] == arg_label):
+                    loop_qty_str = loop_line_split[arg_col_qty]
+                    loop_dt_str = loop_line_split[arg_col_dt]
+                    loop_qty = Decimal(loop_qty_str)
 
-    #   
-    def _DateTimeRange(self, arg_dt_start, arg_dt_end, arg_interval):
-        pass
+
+                    #   Continue: 2021-01-02T19:17:24AEST decaycalc, parsing of ()-() 'DTS' format datetimes, transform to iso (parseable) format
+                    #loop_dt_str = loop_dt_str.replace(")-(", " ")
+                    #loop_dt_str = loop_dt_str.replace("(", "")
+                    #loop_dt_str = loop_dt_str.replace(")", "")
+
+                    _log.debug("loop_dt_str=(%s)" % str(loop_dt_str))
+                    loop_dt = dateparser.parse(loop_dt_str)
+
+                    _log.debug("loop_qty=(%s)" % str(loop_qty))
+                    _log.debug("loop_dt=(%s)" % str(loop_dt))
+
+                    results_dt.append(loop_dt)
+                    results_qty.append(loop_qty)
+        if (len(results_dt) != len(results_qty)):
+            raise Exception("mismatch, len(results_dt)=(%s), len(results_qty)=(%s)" % (str(len(results_dt)), str(len(results_qty))))
+        _log.debug("len(results)=(%s)" % str(len(results_dt)))
+        return [ results_dt, results_qty ]
+
+
+    #   About: Given a path to gpg encrypted file, decrypt file using system gpg/keychain, raise Exception if file is not decryptable, or if it doesn't exist
+    def _DecryptGPGFileToString(self, arg_path_file):
+    #   {{{
+        _log.debug("arg_path_file:\n%s" % str(arg_path_file))
+        cmd_gpg_decrypt = ["gpg", "-q", "--decrypt", arg_path_file ]
+        p = Popen(cmd_gpg_decrypt, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+        result_data_decrypt, result_stderr = p.communicate()
+        result_str = result_data_decrypt.decode()
+        result_stderr = result_stderr.decode()
+        rc = p.returncode
+        if (rc != 0):
+            raise Exception("gpg decrypt non-zero returncode=(%s), result_stderr=(%s)" % (str(rc), str(result_stderr)))
+        _log.debug("lines=(%s)" % str(result_str.count("\n")))
+        return result_str
+    #   }}}
+
 
 #   }}}1
 
---
+++ tests/test_decaycalc.py
@@ -0,0 +1,78 @@
+#   VIM SETTINGS: {{{3
+#   vim: set tabstop=4 modeline modelines=10 foldmethod=marker:
+#   vim: set foldlevel=2 foldcolumn=3: 
+#   }}}1
+#   {{{3
+import importlib
+import unittest
+import os
+import sys
+import datetime
+import dateutil
+import logging
+import io
+import filecmp
+from subprocess import Popen, PIPE
+from unittest.mock import patch
+from freezegun import freeze_time
+import difflib
+#   pandas must be imported *before* any freezegun fake times are used
+import pandas
+import inspect
+import sys
+import argparse
+import logging
+import traceback
+import dateparser
+import pprint
+#   }}}1
+#   {{{1
+from decaycalc.decaycalc import DecayCalc
+
+#   debug logging
+_log = logging.getLogger('decaycalc')
+_logging_format="%(funcName)s: %(levelname)s, %(message)s"
+_logging_datetime="%Y-%m-%dT%H:%M:%S%Z"
+logging.basicConfig(level=logging.DEBUG, format=_logging_format, datefmt=_logging_datetime)
+
+class Test_CliScan(unittest.TestCase):
+#   {{{
+
+    _test_postfix = "\n"
+    _data_dir = os.environ.get('mld_logs_schedule')
+    decaycalc = DecayCalc()
+    prefix = "Schedule.calc."
+    postfix = ".vimgpg"
+    dt_start = dateparser.parse("2021-01-01T16:08:18AEST")
+    dt_end = dateparser.parse("2021-01-02T16:08:18AEST")
+    label = "D-IR"
+    col_label = 0
+    col_qty = 1
+    col_dt = 3
+    delim = ","
+
+    def test_helloworld(self):
+        pass
+
+    def test_GetFiles_Monthly(self):
+        self.assertTrue(os.path.isdir(self._data_dir), "dir not found: %s" %  self._data_dir)
+        _results = self.decaycalc._GetFiles_Monthly(self._data_dir, self.prefix, self.postfix, self.dt_start, self.dt_end)
+        self.assertTrue(len(_results) > 0, "Expect non-empty _results=(%s)" % str(_results))
+        sys.stderr.write(self._test_postfix)
+
+    def test_ReadData(self):
+        self.assertTrue(os.path.isdir(self._data_dir), "dir not found: %s" %  self._data_dir)
+        located_filepaths = self.decaycalc._GetFiles_Monthly(self._data_dir, self.prefix, self.postfix, self.dt_start, self.dt_end)
+        results_dt, results_qty = self.decaycalc._ReadData(located_filepaths, self.label, self.col_dt, self.col_qty, self.col_label, self.delim)
+        self.assertTrue(len(results_dt) > 0, "Expect non-empty results=(%s)" % results_dt)
+        #print(results_dt)
+        #print(results_qty)
+        sys.stderr.write(self._test_postfix)
+
+
+        
+    
+        
+
+#   }}}
+
